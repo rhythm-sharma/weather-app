4522f3c6e335347a15e379de24bba5d6326cb0a0
rrhythmsharma <rrhythmsharma@gmail.com>
2020-06-02 11:29:43 +0530

    Added: new folder structure

---
+++ src/components/ComponentContainer.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="hello">
+    <h1>
+      Welcome to
+      <img class="logo" alt="logo" src="../assets/Richpanel.png" /> RichPanel
+      waether App
+    </h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br />
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
+        >vue-cli documentation</a
+      >.
+    </p>
+    <Search-bar />
+  </div>
+</template>
+
+<script>
+  import SearchBar from "../components/searchBar/searchBar";
+  export default {
+    name: "ComponentContainer",
+    props: {
+      msg: String,
+    },
+    components: {
+      SearchBar,
+    },
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+
+  .logo {
+    height: 30px;
+    width: 30px;
+  }
+</style>
---
+++ src/components/searchBar/searchBar.vue
@@ -0,0 +1,235 @@
+<template>
+  <div class="RNNXgb">
+    <div class="SDkEP">
+      <div class="iblpc">
+        <div class="hsuHs">
+          <span class="wFncld z1asCe MZy1Rb"
+            ><svg
+              focusable="false"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <path
+                d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"
+              ></path></svg
+          ></span>
+        </div>
+      </div>
+      <div
+        jscontroller="iDPoPb"
+        class="a4bIc"
+        jsaction="h5M12e;input:d3sQLd;blur:jI3wzf"
+      >
+        <div class="pR49Ae gsfi"></div>
+        <input
+          class="gLFyf gsfi"
+          maxlength="2048"
+          name="q"
+          type="text"
+          jsaction="paste:puy29d"
+          aria-autocomplete="both"
+          aria-haspopup="false"
+          autocapitalize="off"
+          autocomplete="off"
+          autocorrect="off"
+          role="combobox"
+          spellcheck="false"
+          title="Search"
+          aria-label="Search"
+          data-ved="0ahUKEwjKvqaIsOLpAhXXxzgGHYhoDuMQ39UDCAk"
+        />
+      </div>
+      <div class="dRYYxd">
+        <div
+          jscontroller="J5Ptqf"
+          class="clear-button XoaYSb"
+          aria-label="Clear"
+          role="button"
+          jsaction="AVsnlb;rcuQ6b:npT2md"
+          data-ved="0ahUKEwjKvqaIsOLpAhXXxzgGHYhoDuMQ05YFCAo"
+        >
+          <span class="lBbtTb z1asCe rzyADb" tabindex="0" aria-label="Clear"
+            ><svg
+              focusable="false"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <path
+                d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"
+              ></path></svg
+          ></span>
+          <span class="FqnKTc"></span>
+        </div>
+        <div
+          jscontroller="MC8mtf"
+          class="hpuQDe"
+          aria-label="Search by voice"
+          role="button"
+          tabindex="0"
+          jsaction="h5M12e;rcuQ6b:npT2md"
+          data-ved="0ahUKEwjKvqaIsOLpAhXXxzgGHYhoDuMQvs8DCAs"
+        >
+          <svg
+            class="HPVvwb"
+            focusable="false"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="m12 15c1.66 0 3-1.31 3-2.97v-7.02c0-1.66-1.34-3.01-3-3.01s-3 1.34-3 3.01v7.02c0 1.66 1.34 2.97 3 2.97z"
+              fill="#4285f4"
+            ></path>
+            <path d="m11 18.08h2v3.92h-2z" fill="#34a853"></path>
+            <path
+              d="m7.05 16.87c-1.27-1.33-2.05-2.83-2.05-4.87h2c0 1.45 0.56 2.42 1.47 3.38v0.32l-1.15 1.18z"
+              fill="#f4b400"
+            ></path>
+            <path
+              d="m12 16.93a4.97 5.25 0 0 1 -3.54 -1.55l-1.41 1.49c1.26 1.34 3.02 2.13 4.95 2.13 3.87 0 6.99-2.92 6.99-7h-1.99c0 2.92-2.24 4.93-5 4.93z"
+              fill="#ea4335"
+            ></path>
+          </svg>
+        </div>
+      </div>
+    </div>
+    <button
+      class="Tg7LZd"
+      aria-label="Google Search"
+      type="submit"
+      data-ved="0ahUKEwjKvqaIsOLpAhXXxzgGHYhoDuMQ4dUDCAw"
+    >
+      <div class="FAuhyb">
+        <span class="z1asCe MZy1Rb"
+          ><svg
+            focusable="false"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <path
+              d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"
+            ></path></svg
+        ></span>
+      </div>
+    </button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "SearchBar",
+  };
+</script>
+
+<style>
+  .RNNXgb {
+    background: #fff;
+    display: flex;
+    border: 1px solid #dfe1e5;
+    box-shadow: none;
+    height: 39px;
+    width: 690px;
+    border-radius: 24px;
+    z-index: 3;
+    height: 44px;
+    margin: 0 auto;
+  }
+
+  .SDkEP {
+    flex: 1;
+    display: flex;
+    padding: 5px 4px 0 16px;
+    padding-left: 14px;
+  }
+
+  .Tg7LZd {
+    flex: 0 0 auto;
+    padding-right: 13px;
+  }
+
+  .Tg7LZd {
+    height: 44px;
+    width: 44px;
+    background: transparent;
+    border: none;
+    cursor: pointer;
+    flex: 0 0 auto;
+    padding: 0;
+  }
+
+  .iblpc {
+    display: flex;
+    align-items: center;
+    padding-right: 6px;
+    margin-top: -7px;
+  }
+
+  .a4bIc {
+    display: flex;
+    flex: 1;
+    flex-wrap: wrap;
+  }
+
+  .hsuHs {
+    margin: auto;
+  }
+
+  .iblpc span {
+    display: none;
+  }
+  .wFncld {
+    color: #9aa0a6;
+    height: 20px;
+    width: 20px;
+  }
+  .z1asCe {
+    display: inline-block;
+    fill: currentColor;
+    height: 24px;
+    line-height: 24px;
+    position: relative;
+    width: 24px;
+  }
+
+  .a4bIc {
+    display: flex;
+    flex: 1;
+    flex-wrap: wrap;
+  }
+
+  .pR49Ae {
+    color: transparent;
+    flex: 100%;
+    white-space: pre;
+    height: 34px;
+  }
+  .gsfi,
+  .lst {
+    font: 16px arial, sans-serif;
+    line-height: 34px;
+    height: 34px !important;
+  }
+
+  .gLFyf {
+    background-color: transparent;
+    border: none;
+    margin: 0;
+    padding: 0;
+    color: rgba(0, 0, 0, 0.87);
+    word-wrap: break-word;
+    outline: none;
+    display: flex;
+    flex: 100%;
+    -webkit-tap-highlight-color: transparent;
+    margin-top: -37px;
+    height: 34px;
+    font-size: 16px;
+  }
+
+  .dRYYxd {
+    display: flex;
+    flex: 0 0 auto;
+    margin-top: -5px;
+    align-items: stretch;
+    flex-direction: row;
+  }
+</style>
